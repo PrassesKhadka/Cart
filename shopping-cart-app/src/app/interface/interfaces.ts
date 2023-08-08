de2e595a706ac28ceaa2e9e288d43ec1dd2f0fa6
Prasses <khadkaprasses@gmail.com>
2023-08-08 08:15:39 +0545

    Interface for the Product API setup

--- shopping-cart-app/src/app/interface/interfaces.ts
+++ shopping-cart-app/src/app/interface/interfaces.ts
@@ -1,9 +1,24 @@
-export interface Product
-{
-    id:number;
-    name:string;
-    price:number;
-    image_path:string;
+// quicktype.io to generate API interface
+export interface Product {
+    id:          number;
+    title:       string;
+    price:       number;
+    description: string;
+    category:    Category;
+    image:       string;
+    rating:      Rating;
+}
+
+export enum Category {
+    Electronics = "electronics",
+    Jewelery = "jewelery",
+    MenSClothing = "men's clothing",
+    WomenSClothing = "women's clothing",
+}
+
+export interface Rating {
+    rate:  number;
+    count: number;
 }
 
 export interface CartItem
--- shopping-cart-app/src/app/page.tsx
+++ shopping-cart-app/src/app/page.tsx
@@ -4,8 +4,8 @@ import Product_Component from './ui component/product_page';
 import axios from 'axios';
 
 export default async function Home() {
-  const data = await axios.get('https://fakestoreapi.com/products');
-  const products = data.data;
+  const { data } = await axios.get('https://fakestoreapi.com/products');
+  const products = data;
   return (
     <div>
       <Hero />
