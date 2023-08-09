3385dc51cee0151f65ed11468b45651efae50c55
Prasses <khadkaprasses@gmail.com>
2023-08-09 18:27:51 +0545

    Loading and Error while fetching handeled

---
+++ shopping-cart-app/src/app/components/Error.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[80vh] text-center font-bold text-xl">
+      Error Loading page
+      <br></br>
+      Please try again later
+    </div>
+  );
+};
+
+export default Error;
--- shopping-cart-app/src/app/pages/product/page.tsx
+++ shopping-cart-app/src/app/pages/product/page.tsx
@@ -5,20 +5,32 @@ import { Product } from '@/app/interface/interfaces';
 import { useAppDispatch, useAppSelector } from '@/app/redux/store';
 import { useGetProductsQuery } from '@/app/redux/Slice/apiSlice';
 import { setProductArray } from '@/app/redux/Slice/productSlice';
+import Loader from '@/app/components/Loader';
+import Error from '@/app/components/Error';
 
 const Product_Component = () => {
   const dispatch = useAppDispatch();
-  const { data } = useGetProductsQuery('');
+  const { data, isLoading, isError } = useGetProductsQuery('');
+  // fetching the data and dispatching it to array in the product state
   data && dispatch(setProductArray(data));
   const product = useAppSelector((state) => state.product.productItem);
   return (
     <div>
-      <h3>Products:</h3>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
-        {product.map((element: Product) => (
-          <ProductCard key={element.id} element={element} />
-        ))}
-      </div>
+      {isError && <Error />}
+      {!isLoading && !isError && (
+        <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Products:
+        </h2>
+      )}
+      {isLoading && !isError ? (
+        <Loader />
+      ) : (
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
+          {product.map((element: Product) => (
+            <ProductCard key={element.id} element={element} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
