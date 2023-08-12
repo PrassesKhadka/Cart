79c333dbb65af5cb9764dd06021b0e91e3db8d2e
Prasses <khadkaprasses@gmail.com>
2023-08-12 19:25:10 +0545

    mobile responsive navbar complete

--- shopping-cart-app/src/app/components/Navbar.tsx
+++ shopping-cart-app/src/app/components/Navbar.tsx
@@ -1,41 +1,78 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import CartIcon from './CartIcon';
 import { Menu } from 'lucide-react';
 import Image from 'next/image';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Navbar = () => {
+  const [visible, setVisible] = useState(false);
   return (
-    <div className="flex justify-around items-center shadow-xl mb-2 sticky top-0 bg-slate-50">
-      <Menu className="cursor-pointer block sm:hidden " />
-      <Link href="/">
-        <Image src="/image_logo.png" alt="logo" width={150} height={100} />
-      </Link>
-      <nav className="hidden lg:block md:block">
-        <ul className="flex font-bold">
-          <li className="mr-3">
-            <Link href="/pages/products">Home</Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/pages/category/electronics">Electronics</Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/pages/category/jewelery">Jewelery</Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/pages/category/men's clothing">Men's Clothing</Link>
-          </li>
-          <li className="mr-3">
-            <Link href="/pages/category/women's clothing">
-              Womens's Clothing
+    <div>
+      <AnimatePresence>
+        <div className="flex justify-around items-center shadow-xl mb-2 sticky top-0 bg-slate-50">
+          <Menu
+            className="cursor-pointer block sm:hidden "
+            onClick={() => {
+              setVisible(!visible);
+            }}
+          />
+          <Link href="/">
+            <Image src="/image_logo.png" alt="logo" width={150} height={100} />
+          </Link>
+          <nav className="hidden lg:block md:block">
+            <ul className="flex font-bold">
+              <li className="mr-3">
+                <Link href="/pages/products">Home</Link>
+              </li>
+              <li className="mr-3">
+                <Link href="/pages/category/electronics">Electronics</Link>
+              </li>
+              <li className="mr-3">
+                <Link href="/pages/category/jewelery">Jewelery</Link>
+              </li>
+              <li className="mr-3">
+                <Link href="/pages/category/men's clothing">
+                  Men's Clothing
+                </Link>
+              </li>
+              <li className="mr-3">
+                <Link href="/pages/category/women's clothing">
+                  Womens's Clothing
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Link href="/pages/cart">
+            <CartIcon />
+          </Link>
+        </div>
+        {visible && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            className="bg-slate-300 transparent min-h-[100px] min-w-full flex flex-col justify-start items-center text-xl font-extralight pt-2 "
+          >
+            <Link className="pb-3" href="/pages/products">
+              Home
             </Link>
-          </li>
-        </ul>
-      </nav>
-      <Link href="/pages/cart">
-        <CartIcon />
-      </Link>
+            <Link className="pb-3" href="/pages/category/electronics">
+              Electronics
+            </Link>
+            <Link className="pb-3" href="/pages/category/jewelery">
+              Jewelery
+            </Link>
+            <Link className="pb-3" href="/pages/category/men's clothing">
+              Men's clothing
+            </Link>
+            <Link className="pb-3" href="/pages/category/women's clothing">
+              Women's clothing
+            </Link>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
