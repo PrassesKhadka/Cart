cf69de8bac46a9690e7b44448116ddb47558b597
Prasses <khadkaprasses@gmail.com>
2023-08-13 16:15:44 +0545

    mobile responsive

--- shopping-cart-app/src/app/pages/home/page.tsx
+++ shopping-cart-app/src/app/pages/home/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const Hero = () => {
   return (
-    <section className=" w-full sm:h-[250px] min-h-screen flex flex-col justify-center items-center ">
+    <section className=" w-full sm:h-[250px min-h-screen flex flex-col justify-center items-center ">
       <Link href="/pages/products">
         <div className=" uppercase mt-3 bg-blue-400 p-4 rounded-md ">
           Shop Now
@@ -13,7 +13,7 @@ const Hero = () => {
       <img
         src="/hero-image.jpg"
         alt="image"
-        className="h-full max-h-screen lg:h-[800px] "
+        className="h-full max-h-screen lg:h-[800px] xl:h-[600px] "
       ></img>
     </section>
   );
